95088360bec81b3a9a02defc97c7bdf9ed00fe9a
Your Name <you@example.com>
2026-04-27 20:42:39 +0600

    Configure explicit CORS for production frontend

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ const couponRoutes = require('./routes/coupon.routes');
 const orderRoutes = require('./routes/order.routes');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://online-bakery-shop-frontend.vercel.app', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(morgan('dev'));
 
 const { handleStripeWebhook } = require('./controllers/order.controller');
